7bd2be715c0e0397db7ac26436d6903a9a66feaa
Ferdian <ferdian.artwork@gmail.com>
2018-07-18 10:55:55 +0700

    fix

--- bot.js
+++ bot.js
@@ -28,27 +28,52 @@ bot.on('messageCreate', (msg) => {
 		else if (cmd == "info") {
 			bot.createMessage(msg.channel.id, {
                 embed: {
-                    title: "I'm an embed!", // Title of the embed
-                    description: "Here is some more info, with **awesome** formatting.\nPretty *neat*, huh?",
-                    author: { // Author property
-                        name: msg.author.username,
-                        icon_url: msg.author.avatarURL
+                    title: "Aiko-Bot | Information!",
+                    description: "Info about Aiko-Bot",
+                    author: {
+                        name: bot.user.username,
+                        icon_url: bot.user.avatarURL
                     },
-                    color: 0x008000, // Color, either in hex (show), or a base-10 integer
-                    fields: [ // Array of field objects
+                    fields: [
                         {
-                            name: "Some extra info.", // Field title
-                            value: "Some extra value.", // Field
-                            inline: true // Whether you want multiple fields in same line
+                            name: "Node version: ",
+                            value: process.version,
+                            inline: false
                         },
                         {
-                            name: "Some more extra info.",
-                            value: "Another extra value.",
-                            inline: true
+                            name: "Eris version: ",
+                            value: "Eris 0.8.6",
+                            inline: false
+                        },
+                        {
+                            name: "Operating System: ",
+                            value: process.platform,
+                            inline: false
+                        },
+                        {
+                            name: "Memory Usage: ",
+                            value: (process.memoryUsage().heapUsed / 1024 / 1024) + " MB",
+                            inline: false
+                        },
+                        {
+                            name: "Uptime: ",
+                            value: convertMs(bot.uptime),
+                            inline: false
+                        },
+                        {
+                            name: "Prefix: ",
+                            value: prefix,
+                            inline: false
+                        },
+                        {
+                            name: "Bot Author: ",
+                            value: "Ferdian#3422",
+                            inline: false
                         }
                     ],
-                    footer: { // Footer text
-                        text: "Created with Eris."
+                    footer: {
+                    	icon_url: msg.author.AvatarURL,
+                        text: "Command issued by" + msg.author.username
                     }
                 }
             });
@@ -57,3 +82,21 @@ bot.on('messageCreate', (msg) => {
 });
 
 bot.connect();
+
+/////////
+
+function convertMs(millisec) {
+    var seconds = (millisec / 1000).toFixed(1);
+    var minutes = (millisec / (1000 * 60)).toFixed(1);
+    var hours = (millisec / (1000 * 60 * 60)).toFixed(1);
+    var days = (millisec / (1000 * 60 * 60 * 24)).toFixed(1);
+    if (seconds < 60) {
+        return seconds + " Sec";
+    } else if (minutes < 60) {
+        return minutes + " Min";
+    } else if (hours < 24) {
+        return hours + " Hrs";
+    } else {
+        return days + " Days"
+    }
+}
